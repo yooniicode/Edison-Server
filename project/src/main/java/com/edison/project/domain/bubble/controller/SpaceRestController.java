9718c612a26e1d8c1df28daa2b9e82646ad717dc
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-01-19 15:29:45 +0900

    chore: controller mapping 변경

--- project/src/main/java/com/edison/project/domain/bubble/controller/SpaceRestController.java
+++ project/src/main/java/com/edison/project/domain/bubble/controller/SpaceRestController.java
@@ -3,11 +3,13 @@ package com.edison.project.domain.bubble.controller;
 import com.edison.project.domain.bubble.service.PcaReducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/bubbles")
 @RequiredArgsConstructor
-public class SpaceController {
+public class SpaceRestController {
 
     private final PcaReducer pcaReducer;
 
